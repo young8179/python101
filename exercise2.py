78d5999b301f1aff3a6363c940720848029586aa
Heeyoung Song <hsong8179@gmail.com>
2020-08-18 16:05:41 -0400

    exercise1-3

---
+++ exercise2.py
@@ -0,0 +1,4 @@
+name = input("what is your name?")
+greeting = f"welcome, {name}!!"
+
+print(greeting.upper())
---
+++ exercise3.py
@@ -0,0 +1,6 @@
+name = input("what is name?")
+subject = input("what is subject?")
+
+print(f"{name}'s favorite subject in school is {subject}.")
+print(name + "'s favorite subject in school is " + subject + ".")
+

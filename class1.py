0a598ee2f89d403e4bd18259ffc54fca0c09de7a
Heeyoung Song <hsong8179@gmail.com>
2020-08-18 15:45:02 -0400

    python101 class

---
+++ class1.py
@@ -0,0 +1,7 @@
+user_number = input("give me number")
+
+result = float(user_number) + 5
+
+print(result)
+
+## int / float /  str(change number to string)
---
+++ class2.py
@@ -0,0 +1,8 @@
+customer_name = 'Oakley'
+greeting = f'Welcome, {customer_name}'
+print(greeting)
+
+#what is f string?  / f and {} are one set!
+
+greeting1 = "welcome " + customer_name
+print(greeting1) 

74247e4b578c9fde49ea0dd90bd743b3aeed749b
Heeyoung Song <hsong8179@gmail.com>
2020-08-19 09:36:49 -0400

    edited exerciseM-1

--- python101/exerciseM-1.py
+++ python101/exerciseM-1.py
@@ -1,23 +1,26 @@
-print("Total bill Should be number, and Choice of Level of service: good, fair, bad.")
 
-bill = float(input("Total bill amount: "))
-service = input("Level of service: ")
+try:
+    bill = float(input("Total bill amount: "))
+    service = input("Level of service: ")
 
-if service.lower() == "good" :
-    tip = bill * 0.20
-    bill_total = bill + tip
-    print('Tip amount: ' + "$" + "%.2f" % tip)
-    print('Total amount: ' + "$" + "%.2f" % bill_total)
+    if service.lower() == "good" :
+        tip = bill * 0.20
+        bill_total = bill + tip
+        print('Tip amount: ' + "$" + "%.2f" % tip)
+        print('Total amount: ' + "$" + "%.2f" % bill_total)
 
-elif service.lower() == "fair" :
-    tip = bill * 0.15
-    bill_total = bill + tip
-    print(f'Tip amount: ${"%.2f" % tip}')
-    print(f'Total amount: ${"%.2f" % bill_total}')
-elif service.lower() == "bad" :
-    tip = bill * 0.10
-    bill_total = bill + tip
-    print(f'Tip amount: ${"%.2f" % tip}')
-    print(f'Total amount: ${"%.2f" % bill_total}')
-else:
-    print("Invalid input. Choice of Level of service: good, fair, bad.")
+    elif service.lower() == "fair" :
+        tip = bill * 0.15
+        bill_total = bill + tip
+        print(f'Tip amount: ${"%.2f" % tip}')
+        print(f'Total amount: ${"%.2f" % bill_total}')
+    elif service.lower() == "bad" :
+        tip = bill * 0.10
+        bill_total = bill + tip
+        print(f'Tip amount: ${"%.2f" % tip}')
+        print(f'Total amount: ${"%.2f" % bill_total}')
+    else:
+        print("Invalid input. Choice of Level of service: good, fair, bad.")
+
+except ValueError :
+    print("Invalid input. type a number!")
--- python101/test.py
+++ python101/test.py
@@ -1,6 +1,11 @@
 count = 0
 input_string = input("How high should we count? ")
-MAX = int(input_string)
-while (count < MAX):
-    print(count)        
-    count += 1
+try:
+    MAX = int(input_string)
+    while (count < MAX):
+        is_even = count % 2 == 0
+        if is_even:
+            print(count)
+        count += 1
+except ValueError:
+    print("Please run the program again and type a number!")

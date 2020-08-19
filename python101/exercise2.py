f8f69cd662122cb76bccf10952081885548dd84e
Heeyoung Song <hsong8179@gmail.com>
2020-08-18 22:22:49 -0400

    edited python101 exercise

--- python101/exercise2.py
+++ python101/exercise2.py
@@ -1,7 +1,4 @@
 name = input("what is your name?")
-greeting = f"welcome, {name}!!"
-number_name = "your name has " + str(len(name)) + " letters in it! awesome!"
-number_name1 = f"your name has {len(name)} letters in it! awesome!"
+greeting = f"welcome, {name}!! \nyour name has {str(len(name))} letters in it! awesome!"
+
 print(greeting.upper())
-print(number_name.upper())
-print(number_name1.upper())

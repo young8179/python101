9fa451cc4534e1a152bc5bcb4113d20ee87ef4d5
Heeyoung Song <hsong8179@gmail.com>
2020-08-24 16:06:00 -0400

    added python105

---
+++ python105(dictionaries)/1-phonebook.py
@@ -0,0 +1,13 @@
+phonebook_dict = {
+  'Alice': '703-493-1834',
+  'Bob': '857-384-1234',
+  'Elizabeth': '484-584-2923'
+}
+
+print(phonebook_dict.get("Elizabeth"))
+phonebook_dict['Kareem'] = '938-489-1234' 
+print(phonebook_dict)
+del phonebook_dict['Alice']
+print(phonebook_dict)
+phonebook_dict['Bob'] = '968-345-2345'
+print(phonebook_dict)
